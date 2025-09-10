ae5569c24db7513b4a5ba4473eeb8f310c03a0bd
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-09-10 13:46:11 -0600

    improved test timer script

---
+++ scripts/testTimerScript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script will be used to give me audio cues for when the test starts and when the timer is done.
+
+echo "Test Timer Script started"
+
+# Function to display countdown
+countdown() {
+    local duration=$1
+    local message=$2
+    
+    echo "$message"
+    for ((i=duration; i>0; i--)); do
+        printf "\rTime remaining: %02d:%02d" $((i/60)) $((i%60))
+        sleep 1
+    done
+    printf "\rTime remaining: 00:00\n"
+}
+
+# Countdown to test start (60 seconds)
+countdown 60 "Starting countdown to test..."
+
+# Play sound and announce test start
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga && echo "Test has started!"
+
+# Countdown to test end (200 seconds)
+countdown 200 "Test in progress - countdown to end..."
+
+# Play sound and announce test end
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga && echo "Test has ended!"
